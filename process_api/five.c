fd34c4f245554d9522b176c5efb3ceeac478c084
Samson Wong <samswong@amazon.com>
2018-01-17 05:20:41 +0000

    Process API #5

---
+++ process_api/five.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/*
+ * Useful explanation: https://stackoverflow.com/questions/5769734/what-are-the-different-versions-of-exec-used-for-in-c-and-c
+ * Examples: http://www.yolinux.com/TUTORIALS/ForkExecProcesses.html
+ */
+int
+main(int argc, char *argv[])
+{
+    printf("argv[0]: %s\n", argv[0]);
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+        int wc = wait(NULL);
+        printf("I am still a child (wc:%d)\n", wc);
+    } else {
+        // parent goes down this path (original process)
+        int wc = wait(NULL);
+        printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
+               rc, wc, (int) getpid());
+    }   
+    return 0;
+}
+
+// wait returns the process id of the child
+// wait returns -1 if used in the child process (the child process doesn't have any of its own children to wait on!)
